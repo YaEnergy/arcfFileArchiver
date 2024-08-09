25ee57419dc7e4bff5fe8204b56dc5ef30c74554
YaEnergy <kiano.van.cauter@gmail.com>
2024-08-09 20:55:26 +0200

    File Compression using DEFLATE & ENFLATE

--- src/arcf/FileSystem/ArcfFile.cs
+++ src/arcf/FileSystem/ArcfFile.cs
@@ -5,13 +5,15 @@
         public string Name;
 
         public long StartDataPosition = 0;
-        public long DataLength = 0;
+        public long DeflatedDataLength = 0;
+        public long FullDataLength = 0;
 
-        public ArcfFile(string name, long startDataPosition, long dataLength)
+        public ArcfFile(string name, long startDataPosition, long deflatedDataLength, long fullDataLength)
         {
             Name = name;
             StartDataPosition = startDataPosition;
-            DataLength = dataLength;
+            DeflatedDataLength = deflatedDataLength;
+            FullDataLength = fullDataLength;
         }
     }
 }
